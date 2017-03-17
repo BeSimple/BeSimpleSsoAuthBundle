25513da23db950b84c4dcf87b2df70acce2326fa
Thibaut Salanon <thlbaut@users.noreply.github.com>
2017-03-17 16:33:04 +0100

    Update for Symfony 3
    ContainerAware was removed in Symfony 3.x.

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -2,11 +2,26 @@
 
 namespace BeSimple\SsoAuthBundle\Controller;
 
-use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class Controller extends ContainerAware
+class Controller implements ContainerAwareInterface
 {
+    /**
+     * @var ContainerInterface $container
+     */
+    private $container;
+
+    /**
+     * Sets the container.
+     *
+     * @param ContainerInterface|null $container A ContainerInterface instance or null
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container=$container;
+    }
+
     protected function render($view, array $parameters)
     {
         return $this
